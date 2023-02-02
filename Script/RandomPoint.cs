72080aff7b1ec69c52bd4b0b8a380af4b5362839
Yam_K <122723235+SeungHee-K@users.noreply.github.com>
2023-02-02 22:59:08 +0900

    Update RandomPoint.cs

--- Script/RandomPoint.cs
+++ Script/RandomPoint.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class RandomPoint : MonoBehaviour
 {
-
     public GameObject[] Point;
 
     int RandomPoints;
 
-
-
-
     void Start()
     {
-        RandomPoints = Random.Range(0, Point.Length);
-
-
-        
+        RandomPoints = Random.Range(0, Point.Length);      
         Debug.Log(RandomPoints);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() {}
 }
